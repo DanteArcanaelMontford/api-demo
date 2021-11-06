a63478e4fe3e0f8ec965b2832372d1021c4f9af3
ghost <dantearcanaelmontford@gmail.com>
2021-11-06 03:12:55 -0300

    adding a create Controller and route using method POST

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -10,6 +10,28 @@ async function get(req, res) {
   res.send(products);
 }
 
+async function post(req, res) {
+  const { name, brand, price } = await req.body;
+
+  const createProduct = new ProductsModel({
+    name,
+    brand,
+    price,
+  });
+
+  createProduct.save((err) => {
+    if (err) {
+      console.error("Error while try to create product:", err);
+      return;
+    }
+  });
+
+  res.send({
+    created: true,
+  });
+}
+
 module.exports = {
   get,
+  post,
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const ProductController = require("../controllers/productController");
 
 router.get("/products/:id?", ProductController.get);
+router.post("/products/create", ProductController.post);
 
 module.exports = router;
